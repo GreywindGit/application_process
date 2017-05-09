70bdcc32681a0c4054c34a9edfc67468b7a4c4cf
GreywindGit <GreywindGit@users.noreply.github.com>
2017-05-09 14:30:41 +0200

    Layout brush up (add header)

--- ui.py
+++ ui.py
@@ -16,32 +16,43 @@ def menu():
         except ValueError:
             menu_choice = 0
     if menu_choice == 1:
+        header = 'Mentors'
         table = dal.get_mentors_list()
     elif menu_choice == 2:
+        header = 'Nicknames of mentors in Miskolc'
         table = dal.get_mentor_nicks()
     elif menu_choice == 3:
+        header = 'Carol\'s phone number'
         table = dal.search_by_first_name()
     elif menu_choice == 4:
+        header = 'Applicant\'s phone number'
         table = dal.search_by_email()
     elif menu_choice == 5:
+        header = 'New applicants'
         table = dal.add_new_applicant()
     elif menu_choice == 6:
+        header = 'Updated applicant data'
         table = dal.change_applicant_data()
     elif menu_choice == 7:
         table = dal.remove_applicant()
     else:
         pass
     if menu_choice != 8:
-        display_table(table)
+        display_table(table, header)
         restart()
 
 
-def display_table(table):
+def display_table(table, header=''):
     os.system('clear')
+    print(header)
+    max_row_length = max([len(' '.join(item)) for item in table])
+    separator = max_row_length if max_row_length > len(header) else len(header)
+    print('-' * separator)
     for row in table:
         for item in row:
             print(item, end=' ')
         print()
+    print()
 
 
 def restart():
