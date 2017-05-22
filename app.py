c3f0d229e3c9dccaac28594819f8384fc5bdf843
GreywindGit <GreywindGit@users.noreply.github.com>
2017-05-22 14:57:53 +0200

    Add header to tables

--- app.py
+++ app.py
@@ -19,26 +19,30 @@ def index():
 
 @app.route('/mentors')
 def mentors():
+    table_header = ['First name', 'Last name', 'School', 'Country']
     result_set = bll.get_mentors()
-    return render_template('mentors.html', menu_items=menu_items, table=result_set)
+    return render_template('mentors.html', menu_items=menu_items, table_header=table_header, table=result_set)
 
 
 @app.route('/all-school')
 def all_school():
+    table_header = ['First name', 'Last name', 'School', 'Country']
     result_set = bll.get_all_school()
-    return render_template('all-school.html', menu_items=menu_items, table=result_set)
+    return render_template('all-school.html', menu_items=menu_items, table_header=table_header, table=result_set)
 
 
 @app.route('/mentors-by-country')
 def mentors_by_country():
+    table_header = ['Country', '# of mentors']
     result_set = bll.get_mentors_by_country()
-    return render_template('mentors-by-country.html', menu_items=menu_items, table=result_set)
+    return render_template('mentors-by-country.html', menu_items=menu_items, table_header=table_header, table=result_set)
 
 
 @app.route('/contacts')
 def get_contacts():
+    table_header = ['School', 'First name', 'Last name']
     result_set = bll.get_contacts()
-    return render_template('contacts.html', menu_items=menu_items, table=result_set)
+    return render_template('contacts.html', menu_items=menu_items, table_header=table_header, table=result_set)
 
 
 if __name__ == '__main__':
