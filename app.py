0f708001cdb51468ecc0b858d1f855379ae7fb2a
GreywindGit <GreywindGit@users.noreply.github.com>
2017-05-22 13:30:06 +0200

    Add mentors route and menu items to server app

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+import bll
 
 app = Flask(__name__)
 
 
+menu_items = [('mentors', 'mentors'),
+              ('all-school', 'all-school'),
+              ('mentors by country', 'mentors-by-country'),
+              ('contacts', 'contacts'),
+              ('applicants', 'applicants'),
+              ('mentors and applicants', 'mentors-and-applicants')]
+
+
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('index.html', menu_items=menu_items)
+
+
+@app.route('/mentors')
+def mentors():
+    result_set = bll.get_mentors()
+    return render_template('mentors.html', menu_items=menu_items, table=result_set)
 
 
 if __name__ == '__main__':
