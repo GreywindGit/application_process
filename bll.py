7a92eb75d27490efefa1fd2013046662ddf030a5
GreywindGit <GreywindGit@users.noreply.github.com>
2017-05-22 14:11:48 +0200

    Add get_all_school and get_mentors_by_city functions to bll

--- bll.py
+++ bll.py
@@ -8,3 +8,23 @@ def get_mentors():
                   ORDER BY mentors.id;"
     result_set = dal.get_data_from_table(sql_string)
     return result_set
+
+
+def get_all_school():
+    sql_string = "SELECT mentors.first_name, mentors.last_name, schools.name, schools.country\
+                  FROM mentors\
+                  FULL JOIN schools ON mentors.city = schools.city\
+                  ORDER BY mentors.id;"
+    result_set = dal.get_data_from_table(sql_string)
+    return result_set
+
+
+def get_mentors_by_country():
+    sql_string = "SELECT schools.country, count(mentors.id)\
+                  FROM mentors\
+                  JOIN schools ON mentors.city = schools.city\
+                  GROUP BY schools.country\
+                  ORDER BY schools.country;"
+    result_set = dal.get_data_from_table(sql_string)
+    return result_set
+
