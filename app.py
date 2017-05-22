7599f59400ec485d9dd30c5ebf45a21c63864cbe
GreywindGit <GreywindGit@users.noreply.github.com>
2017-05-22 14:11:18 +0200

    Add all-school and mentors-by-country routes to server app

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import bll
 app = Flask(__name__)
 
 
-menu_items = [('mentors', 'mentors'),
-              ('all-school', 'all-school'),
-              ('mentors by country', 'mentors-by-country'),
-              ('contacts', 'contacts'),
-              ('applicants', 'applicants'),
-              ('mentors and applicants', 'mentors-and-applicants')]
+menu_items = [('mentors', 'Mentors'),
+              ('all-school', 'All School'),
+              ('mentors-by-country', 'Mentors by Country'),
+              ('contacts', 'Contacts'),
+              ('applicants', 'Applicants'),
+              ('mentors-and-applicants', 'Mentors and Applicants')]
 
 
 @app.route('/')
@@ -21,6 +21,18 @@ def index():
 def mentors():
     result_set = bll.get_mentors()
     return render_template('mentors.html', menu_items=menu_items, table=result_set)
+
+
+@app.route('/all-school')
+def all_school():
+    result_set = bll.get_all_school()
+    return render_template('all-school.html', menu_items=menu_items, table=result_set)
+
+
+@app.route('/mentors-by-country')
+def mentors_by_country():
+    result_set = bll.get_mentors_by_country()
+    return render_template('mentors-by-country.html', menu_items=menu_items, table=result_set)
 
 
 if __name__ == '__main__':
